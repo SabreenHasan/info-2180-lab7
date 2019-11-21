aac68a2305747039f8e64d1f77fe1451baf8f7c3
Sabreen Hasan <sabreen_hasan@yahoo.com>
2019-11-20 19:01:24 -0500

    Exercise 2 & 3 Completed

---
+++ world.js
@@ -0,0 +1,51 @@
+window.onload = function{
+	var buttonCities = document.getElementById("cities");
+	var buttonCountry = document.getElementById("country");
+
+	buttonCountry.addEventListener("click", function(e) {
+		e.preventDefault();
+		httpReq = new XMLHttpRequest();
+		var textField = document.getElementById("entries").value;
+
+		if (buttonCountry.checked == true && null != textField) {
+			var url = "world.php?country=" + textField; 
+			httpReq.onreadystatechange = loadPage;
+			httpReq.open('GET', url);
+			httpReq.send();
+		}
+		if else (buttonCountry.checked == true && null == textField) {
+			var url = "world.php?allcountries=";
+			httpReq.onreadystatechange = loadPage;
+			httpReq.open('GET', url);
+			httpReq.send();
+		}
+	})
+
+	buttonCities.addEventListener("click", function(e) {
+		e.preventDefault();
+		httpReq = new XMLHttpRequest();
+		var textField = document.getElementById("entries");
+
+		if (buttonCities.checked == true && null != textField) {
+			var url = "world.php?cities=" + textField;
+			httpReq.onreadystatechange = loadPage;
+			httpReq.open('GET', url);
+			httpReq.send();
+		}
+	})
+
+	function loadPage {
+		if (httpReq.readyState === XMLHttpRequest.DONE)
+		{
+			if (httpReq.status === 200) 
+			{
+				var response = httpReq.responseText;
+				document.getElementById("result").innerHTML = response;
+			}
+			else
+			{
+				alert("ERROR!");
+			}
+		}
+	}
+}
