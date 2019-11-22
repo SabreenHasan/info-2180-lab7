21153cd94731b759d8dfa5163dde4a93a3a25ef8
Sabreen Hasan <sabreen_hasan@yahoo.com>
2019-11-21 22:58:39 -0500

    Exercise 4 & 5 completed

--- world.php
+++ world.php
@@ -1,11 +1,17 @@
 <?php
+header("Access-Control-Allow-Origin:*");
 $host = getenv('IP');
-$username = 'lab7_user';
-$password = '';
+$username = 'Sabie0498';
+$password = '$1nG/_3n/_uv1n1t';
 $dbname = 'world';
+$country = $_GET['country'];
 
-$conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
-$stmt = $conn->query("SELECT * FROM countries");
+
+//Create connection
+$conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4",$username,$password);
+
+//Check connection
+$stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
 
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
@@ -15,3 +21,60 @@ $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
   <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
 <?php endforeach; ?>
 </ul>
+
+<!--sanitization done in js file-->
+<?php if(isset($_GET['country']) && !isset($_GET['context'])):?>
+<?php
+
+$stmt2 = $conn->queryfetchAll(PDO::FETCH_ASSOC);
+?>
+<table style="width:100%">
+	<tr>
+		<th>Name</th>
+		<th>Continent</th>
+		<th>Independence</th>
+		<th>Head of State</th>
+	</tr>
+	<?php foreach ($contents as $row):?>
+	<tr>
+		<td><?=$row['name']?></td>
+		<td><?=$row['continent']?></td>
+		<td><?=$row['independence_year']?></td>
+		<td><?=$row['head_of_state']?></td>
+	</tr>
+	<?php endforeach;?>
+</table>
+
+<?php endif;?>
+<?php if(isset($_GET['context'])&& !empty($_GET['country'])):?>
+<?php
+
+$stmt2 = $conn->prepare("SELECT code FROM countries WHERE name = :name");
+$area = filter_input(INPUT_GET, 'country', FILTER_SANITIZE_SPECIAL_CHARS);
+
+$stmt2->bindParam(':name', $area, PDO::PARAM_STR);
+
+$stmt2->execute();
+$locale = $stmt2->fetch(PDO::FETCH_ASSOC);
+$city = $locale['code'];
+
+$stmt3 = $conn->query("SELECT cities.name, cities.district, cities.population FROM cities INNER JOIN countries ON cities.country_code=countries.code WHERE cities.country_code = '$city'");
+
+$destination = $stmt3->fetchAll(PDO::FETCH_ASSOC);
+?>
+
+<table style="width:100%">
+	<tr>
+		<th>Name</th>
+		<th>District</th>
+		<th>Population</th>
+	</tr>
+	<?php foreach ($destination as $row):?>
+	<tr>
+		<td><?=$row['name']?></td>
+		<td><?=$row['district']?></td>
+		<td><?=$row['population']?></td>
+	</tr>
+	<?php endforeach;?>
+</table>
+<?php endif;?>
